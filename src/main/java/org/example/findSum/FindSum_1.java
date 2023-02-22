138ecfc238d2d4ed81d5e0dedf27c879fe6b5e61
decagon <udemekendrick@gmail.com>
2023-02-22 22:35:04 +0100

    FindSum algorithm with time complexity updated.

---
+++ src/main/java/org/example/findSum/FindSum_1.java
@@ -0,0 +1,8 @@
+package org.example.findSum;
+
+public class FindSum_1 {
+
+    public int findSum(int n){
+        return n * (n + 1) / 2;
+    }
+}
---
+++ src/main/java/org/example/findSum/FindSum_2.java
@@ -0,0 +1,12 @@
+package org.example.findSum;
+
+public class FindSum_2 {
+
+    public int findSum(int n){
+        int sum = 0;
+        for (int i = 1; i <= n; i++){
+            sum = sum + i;
+        }
+        return sum;
+    }
+}
