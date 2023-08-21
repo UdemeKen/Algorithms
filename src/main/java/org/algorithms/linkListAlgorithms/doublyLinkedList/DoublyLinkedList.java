5037f407d5c8501753c03f50abe4d87c608a0bc6
decagon <udemekendrick@gmail.com>
2023-08-21 17:23:09 +0100

    insertFirst method in doublyLinkedList added

--- src/main/java/org/algorithms/linkListAlgorithms/doublyLinkedList/DoublyLinkedList.java
+++ src/main/java/org/algorithms/linkListAlgorithms/doublyLinkedList/DoublyLinkedList.java
@@ -27,4 +27,60 @@ public class DoublyLinkedList {
     public int length(){
         return length;
     }
+
+    public void insertLast(int value){
+        ListNode newNode = new ListNode(value);
+        if (isEmpty()){
+            head = newNode;
+        }else {
+            tail.next = newNode;
+        }
+        newNode.previous = tail;
+        tail = newNode;
+        length++;
+    }
+
+    public void insertFirst(int value){
+        ListNode newNode = new ListNode(value);
+        if (isEmpty()){
+            tail = newNode;
+        }else {
+            head.previous = newNode;
+        }
+        newNode.next = head;
+        head = newNode;
+        length++;
+    }
+
+    public void displayForward(){
+        if (head == null){
+            return;
+        }
+        ListNode temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void displayBackward(){
+        if (tail == null){
+            return;
+        }
+        ListNode temp = tail;
+        while(temp != null){
+            System.out.print(temp.data + "<--");
+            temp = temp.previous;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.insertFirst(1);
+
+        dll.displayForward();
+        dll.displayBackward();
+    }
 }
