94a3cd5cccb59d681c854e5495047c38995f3af4
decagon <udemekendrick@gmail.com>
2024-11-15 07:40:00 +0100

    stack added

---
+++ src/main/java/org/algorithms/stack/Stack.java
@@ -0,0 +1,29 @@
+package org.algorithms.stack;
+
+public class Stack {
+    private ListNode top;
+    private int length;
+
+    public class ListNode {
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Stack() {
+        top = null;
+        length = 0;
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+}
